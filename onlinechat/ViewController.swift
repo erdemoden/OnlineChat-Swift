cf9b7753279927752e723a4c012eeeaa5123baa9
erdemoden <erdemoden5@gmail.com>
2022-01-29 15:52:58 +0300

    Initial Commit

---
+++ onlinechat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  onlinechat
+//
+//  Created by erdem öden on 29.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
