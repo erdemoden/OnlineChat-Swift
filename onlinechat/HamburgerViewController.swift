209499800a8245f00bfd3a477aab1af131fee878
erdemoden <erdemoden5@gmail.com>
2022-01-29 23:45:49 +0300

    first commit

---
+++ onlinechat/HamburgerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HamburgerViewController.swift
+//  onlinechat
+//
+//  Created by erdem öden on 29.01.2022.
+//
+
+import UIKit
+
+class HamburgerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ onlinechat/View/ChatTables.swift
@@ -0,0 +1,23 @@
+//
+//  ChatTables.swift
+//  onlinechat
+//
+//  Created by erdem öden on 29.01.2022.
+//
+
+import UIKit
+
+class ChatTables: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
