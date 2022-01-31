c56b098265d9b6be28235a19982002ec2a7ca069
erdemoden <erdemoden5@gmail.com>
2022-01-31 23:19:19 +0300

    hamburger menu opening

--- onlinechat/ViewController.swift
+++ onlinechat/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     private var isHamburgerMenuShown:Bool = false
+    private var BeginPoint:CGFloat = 0.0;
+    private var Difference:CGFloat = 0.0;
     @IBOutlet weak var HamburgerRight: NSLayoutConstraint!
     @IBOutlet weak var TitleBackGround: UIView!
     @IBOutlet weak var ChatTables: UITableView!
@@ -72,6 +74,57 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
             }
         }
     }
+    
+    
+    //-MARK: Hamburger Menu Movement Code
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(isHamburgerMenuShown){
+            if let touch = touches.first{
+                let location = touch.location(in: HamburgerBackView);
+                BeginPoint = location.x;
+            }
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(isHamburgerMenuShown){
+            if let touch = touches.first{
+                let location = touch.location(in: HamburgerBackView);
+                let DifferenceFromBeginPoint = BeginPoint-location.x;
+                if(DifferenceFromBeginPoint > 0 && DifferenceFromBeginPoint < 280){
+                    self.HamburgerRight.constant = -DifferenceFromBeginPoint;
+                    self.Difference = DifferenceFromBeginPoint;
+                    self.HamburgerBackView.alpha = 0.75-(0.75*DifferenceFromBeginPoint/280);
+                }
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if(isHamburgerMenuShown){
+            if let touch = touches.first{
+                let location = touch.location(in: HamburgerBackView);
+                
+                // Close Hamburger Menu
+                if(Difference>140){
+                    UIView.animate(withDuration: 0.1) {
+                        self.HamburgerRight.constant = -280;
+                    } completion: { (status) in
+                        self.isHamburgerMenuShown = false;
+                        self.HamburgerBackView.isHidden = true;
+                    }
+                }
+                // Open Hamburger Menu
+                else{
+                    UIView.animate(withDuration: 0.1){
+                        self.HamburgerRight.constant = 0;
+                    } completion: { (status) in
+                        self.isHamburgerMenuShown = true;
+                        self.HamburgerBackView.isHidden = false;
+                    }
+                }
+            }
+        }
+    }
 }
 
