d2080ff75ddcbc265bf8a235ba13fe1e90795b2e
erdemoden <erdemoden5@gmail.com>
2022-02-06 19:45:39 +0300

    giriş sayfası

---
+++ onlinechat/LoginScene/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  onlinechat
+//
+//  Created by erdem öden on 6.02.2022.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    @IBOutlet weak var TitleBackGround: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        TitleBackGround.layer.cornerRadius = 20;
+        let Gesture = UITapGestureRecognizer(target: self.view, action:#selector(UIView.endEditing));
+        view.addGestureRecognizer(Gesture);
+    }
+}
