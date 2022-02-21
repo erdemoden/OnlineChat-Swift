7a2284dab3e49f9190586da8ebe098d213b8a2ed
erdemoden <erdemoden5@gmail.com>
2022-02-21 18:31:28 +0300

    sdds

--- onlinechat/ImagePickScene/Controller/PickController.swift
+++ onlinechat/ImagePickScene/Controller/PickController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class PickController: UIViewController {
+class PickController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
     @IBOutlet weak var ChosenImage: UIImageView!
     override func viewDidLoad() {
@@ -16,12 +16,20 @@ class PickController: UIViewController {
     
     }
     @IBAction func PickImage(_ sender: Any) {
-        
+        let Picker = UIImagePickerController();
+        Picker.delegate = self;
+        Picker.sourceType = .photoLibrary;
+        Picker.allowsEditing = true;
+        present(Picker, animated: true, completion: nil);
     }
     @IBAction func AcceptPhoto(_ sender: Any) {
         
     }
     @IBAction func Cancel(_ sender: Any) {
-        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        ChosenImage.image = info[.editedImage] as? UIImage
+        self.dismiss(animated: true, completion: nil);
     }
 }
